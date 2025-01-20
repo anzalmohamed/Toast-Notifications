a73d7eef92beb46780067967a8a6d367a7b6abc8
anzalmohamed <anzaldidah@gmail.com>
2025-01-20 10:59:37 +0300

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let toastBox = document.getElementById("toastBox");
-function showToast() {
+let successMsg = '<i class="fa-solid fa-circle-check"></i> successfully submited';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(msg) {
   let toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerHTML = "success";
+  toast.innerHTML = msg;
   toastBox.appendChild(toast);
+
+  if (msg.includes('error')){
+    toast.classList.add('error');
+}
+if (msg.includes('Invalid')){
+    toast.classList.add('invalid');
+}
+
 }
