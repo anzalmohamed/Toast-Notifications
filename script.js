adfe7b26572c453cde900a86a8c9ad73ff1f677f
anzalmohamed <anzaldidah@gmail.com>
2025-01-20 10:31:12 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let toastBox = document.getElementById("toastBox");
+function showToast() {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = "success";
+  toastBox.appendChild(toast);
+}
